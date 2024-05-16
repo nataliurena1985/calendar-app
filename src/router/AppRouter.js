c681daef6373d2911998feb7618c94a002e1859c
claudia <claudianataliurena@gmail.com>
2024-05-16 11:45:30 -0300

    rutas

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { CalendarScreen } from "../components/calendar/CalendarScreen";
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/" element={<CalendarScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
