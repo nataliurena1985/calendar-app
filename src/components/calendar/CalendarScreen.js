576bf1103d811ad9c5390c7d3dca4c1c0ab522fe
claudia <claudianataliurena@gmail.com>
2024-05-16 19:07:14 -0300

    agregamos el calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { Calendar, momentLocalizer } from "react-big-calendar";
+
+import moment from "moment";
+
+const localizer = momentLocalizer(moment);
 
 export const CalendarScreen = () => {
+
+
+
+
+
+
+const events=[{
+  title:"cumpleaños del jefe",
+  start:moment().toDate(),
+  end:moment().add(2,"hours").toDate(),
+  bgcolor:"fafafa"
+}]
+
+
+
   return (
-    <div>CalendarScreen</div>
-  )
-}
+    <div className="calendar-screen">
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { CalendarApp } from "./CalendarApp";
 
-
+import "./styles.css";
 const divRoot = document.querySelector("#root");
 
 ReactDOM.render(<CalendarApp />, divRoot);
