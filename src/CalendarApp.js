476209e9bf993b81a987f08a4dc11640b26793df
claudia <claudianataliurena@gmail.com>
2024-05-16 11:29:50 -0300

    estructura de carpetas

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,7 +1,6 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter';
 
 export const CalendarApp = () => {
-  return (
-    <div>CalendarApp</div>
-  )
+  return <AppRouter />;
 }
