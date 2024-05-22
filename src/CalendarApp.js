d4c96a53f86b9468f9d0459c50d1ed7412e3ffa7
claudia <claudianataliurena@gmail.com>
2024-05-22 12:11:42 -0300

    resuelvo el poblema de store  poniendo llaves en store en tumbh

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import { AppRouter } from './router/AppRouter';
+import React from "react";
+import { AppRouter } from "./router/AppRouter";
 
 import { Provider } from "react-redux";
-// import { store } from "./store/store";
-
-
-
+import { store } from "./store/store";
 
 export const CalendarApp = () => {
   return (
-   // <Provider store={store}>
+    <Provider store={store}>
       <AppRouter />
-    //</Provider>
+    </Provider>
   );
-}
+};
